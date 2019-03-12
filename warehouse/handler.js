5af082ba25795e60173de1992862f46cbe8e36ed
doan.dominh <doan.dominh@gameloft.com>
2019-03-12 11:01:02 +0700

    - added APIs and deploy script

--- warehouse/handler.js
+++ warehouse/handler.js
@@ -1,14 +1,53 @@
-'use strict';
+ 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const addProduct = require('./handlers/create');
+const listProduct = require('./handlers/list');
+const viewProduct = require('./handlers/view');
+const removeProduct = require('./handlers/remove');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const create = (event, context, callback) => {
+	const data = JSON.parse(event.body);
+	addProduct(data)
+		.then(result => {
+			const response = { body: JSON.stringify(result) };
+			callback(null, response);
+		})
+		.catch(callback);
+};
+
+const list = (event, context, callback) => {	
+	listProduct()
+		.then(result => {
+			const response = { body: JSON.stringify(result) };
+			callback(null, response);
+		})
+		.catch(callback);
+};
+
+const view = (event, context, callback) => {
+	const id = event.pathParameters.id;
+	viewProduct(id)
+		.then(result => {
+			const response = { body: JSON.stringify(result) };
+			callback(null, response);
+		})
+		.catch(callback);
+};
+
+const remove = (event, context, callback) => {
+	const id = event.pathParameters.id;
+	removeProduct(id)
+		.then(result => {
+			const response = { body: JSON.stringify(result) };
+			callback(null, response);
+		})
+		.catch(callback);
+};
+
+
+module.exports = {
+	create,
+	list,
+	view,
+	remove,
 };
